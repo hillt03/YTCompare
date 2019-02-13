0d0726b5616c714eef7953c88f80f374fe4f05eb
Tim Hill <hillt03@yahoo.com>
2019-02-13 17:21:22 -0500

    songs_and_urls dict operational, added readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-YTCompare
+#YTCompare
+Compare YouTube playlist to files on your local machine
+and choose to download any files that aren't already stored locally.
+
+##Dependencies
+python3
+
+`pip install requests` or `python -m pip install requests`
+
+##Installation
+You will have to provide a YouTube Data v3 API key.
+
+Go [here](https://developers.google.com/youtube/v3/getting-started) for information on obtaining a key.
+
+Download `YTCompare.py` and run with `python YTCompare.py` and follow the prompts.
+
--- YTCompare.py
+++ YTCompare.py
@@ -1,8 +1,12 @@
+"""
+YTCompare
+"""
 import requests
 """
 API Reference URL
 https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLqq-6Pq4lTTZh5U8RbdXq0WaYvZBz2rbn&key=AIzaSyCg3WitBUQl5ifC2QygQaZUPOSRMKfSD5E&nextpage=1
 """
+
 
 def gather_data(playlist_id_, page_token_=None):
     """Receive playlist data from YouTube's API"""
@@ -23,13 +27,13 @@ def gather_data(playlist_id_, page_token_=None):
 
 
 if __name__ == "__main__":
-    # domain = "www.youtube.com/"
+    domain = "www.youtube.com/watch?v="
     #playlist_id = "PLt-RfMTsSUDF0vjKuKmVS9r0xpwjI3vyj" # Mine
     playlist_id = "PLdy_Z-y66LeLNgbrAq6fXnuO42Lolm_dD" # Jordan's
     data = gather_data(playlist_id)
     page_token = data['nextPageToken']
 
-    # songs_and_urls = {}
+    songs_and_urls = {}
     next_page_available = True
     use_token = False
     song_count = 0
@@ -44,8 +48,20 @@ if __name__ == "__main__":
         except KeyError:
             break
         for item in data['items']:
-            print(item['snippet']['title'])
+            songs_and_urls["Song " + str(song_count)] = {
+                                                         "Title": item['snippet']['title'],
+                                                         "URL": domain + item['snippet']['resourceId']['videoId'],
+                                                        }
+            #print(item['snippet']['title'])
             song_count += 1
         use_token = True
     print("Song count: " + str(song_count))
+
+    for song in songs_and_urls:
+        print("=====================================")
+        print(song)
+        print("Title: " + songs_and_urls[song]['Title'])
+        print("URL: " + songs_and_urls[song]["URL"])
+
+
 
